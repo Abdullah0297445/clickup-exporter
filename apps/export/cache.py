b4dfe1c8e87fc2de87b43628b66f2f97ea56500d
Abdullah Ajmal <abdullah.ajmal.7@hotmail.com>
2025-10-16 09:45:49 +0500

    Refactor cache version generation to use `datetime.timedelta` instead of `timezone.timedelta`.

--- apps/export/cache.py
+++ apps/export/cache.py
@@ -1,4 +1,5 @@
 from django.utils import timezone
+from datetime import timedelta
 
 
 def get_current_cache_version():
@@ -9,4 +10,5 @@ def get_old_cache_version(**timedelta_kwargs):
     if not timedelta_kwargs:
         timedelta_kwargs = {"days": 30}
 
-    return (timezone.now() - timezone.timedelta(**timedelta_kwargs)).strftime("%Y%m%d")
+    dt = timezone.now() - timedelta(**timedelta_kwargs)
+    return dt.strftime("%Y%m%d")
