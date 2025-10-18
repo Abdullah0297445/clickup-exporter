f2e232632046d4ef4302d6b31687359faeb6474a
Abdullah Ajmal <abdullah.ajmal.7@hotmail.com>
2025-10-18 18:02:15 +0500

    Gracefully restart stack during deployment in `after_install.sh`
    - Updated `after_install.sh` to stop the current stack before bringing up the new stack.
    - Simplified Docker Compose commands for starting the stack.

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -8,10 +8,12 @@ REGION=${AWS_DEFAULT_REGION:-ap-southeast-1}
 log "Signing in to ECR"
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 
+log "Stopping current stack"
+docker compose -f docker-compose.prod.yml --env-file .env --env-file .image_uri_env down --remove-orphans
 log "Pulling images"
 docker compose -f docker-compose.prod.yml --env-file .env --env-file .image_uri_env pull
-log "Bringing stack up"
-docker compose -f docker-compose.prod.yml --env-file .env --env-file .image_uri_env up -d --remove-orphans
+log "Bringing new stack up"
+docker compose -f docker-compose.prod.yml --env-file .env --env-file .image_uri_env up -d
 
 log "Deployment complete."
 exit 0
