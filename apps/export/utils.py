b323ecdb0ce0abb21432df23991f23c37d09ece3
Abdullah Ajmal <abdullah.ajmal.7@hotmail.com>
2025-11-11 10:21:19 +0500

    Refactor export utility for improved key handling and error resilience
    - Updated `get_all_keys` to ensure type consistency with return annotation.
    - Enhanced decoding logic in `get_all_keys` for proper function usage.
    - Refined `get_earliest_version` and `get_latest_version` to eliminate redundant calls and improve error handling.
    - Simplified logic for determining earliest and latest cache versions.

--- apps/export/utils.py
+++ apps/export/utils.py
@@ -27,24 +27,32 @@ def verify_bearer_token(request: HttpRequest):
     return True, None
 
 
-def get_all_keys(team_id):
+def get_all_keys(team_id) -> list:
     key_pattern = cache.make_and_validate_key(key=team_id, version="*")
-    keys = [k.decode for k in cache._cache.get_client(key_pattern).scan_iter(match=key_pattern)]
+    keys = [k.decode() for k in cache._cache.get_client(key_pattern).scan_iter(match=key_pattern)]
     return keys
 
 
 def get_earliest_version(team_id):
+    keys = get_all_keys(team_id)
+
+    if len(keys) <= config.KEEP_LAST_N_EXPORTS:
+        return None
+
     try:
-        keys = get_all_keys(team_id)
-        if len(keys) <= config.KEEP_LAST_N_EXPORTS:
-            return None
-        return min(get_all_keys(team_id)).split(":")[1]
+        earliest = min(keys)
     except ValueError:
         return None
+
+    return earliest.split(":")[1]
 
 
 def get_latest_version(team_id):
+    keys = get_all_keys(team_id)
+
     try:
-        return max(get_all_keys(team_id)).split(":")[1]
+        latest = max(keys)
     except ValueError:
         return None
+
+    return latest.split(":")[1]
