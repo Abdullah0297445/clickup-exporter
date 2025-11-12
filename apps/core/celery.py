36ff813a73b86aaaf1cba3d679fb93713deb0ad9
Abdullah Ajmal <abdullah.ajmal.7@hotmail.com>
2025-11-12 08:29:55 +0500

    Update ClickUp task schedule to run every hour
    - Replaced `crontab` schedule with `timedelta` for better clarity and time interval management.

--- apps/core/celery.py
+++ apps/core/celery.py
@@ -1,6 +1,6 @@
 import os
-from celery.schedules import crontab
 from celery import Celery
+from datetime import timedelta
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.dev")
@@ -19,6 +19,6 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     "fetch_clickup_data_and_persist": {
         "task": "export.tasks.fetch_clickup_data_and_persist",
-        "schedule": crontab(minute=0, hour=0),
+        "schedule": timedelta(hours=1),
     },
 }
